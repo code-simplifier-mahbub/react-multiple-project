89241c5e1f3cf2d70ce431b9cecf5ff5bcad4d57
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2024-01-10 23:45:42 +0600

    first commit

---
+++ src/component/Accordian/Accordian.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import data from "./data";
+
+const Accordion = () => {
+  const [selected, setSelected] = useState("");
+  const handleAccordian = (currentId) => {
+    setSelected(currentId === selected ? null : currentId);
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center flex-col">
+      {data && data.length > 0 ? (
+        data.map((dataItem) => (
+          <div key={dataItem.id}>
+            <div
+              onClick={() => handleAccordian(dataItem.id)}
+              className="bg-gray-200 mt-5 w-[600px] p-4 text-md font-bold text-gray-400 rounded-md"
+            >
+              <h3>{dataItem.question} there</h3>
+              <span className="font-bold text-4xl">+</span>
+            </div>
+            {selected !== dataItem.id ? null : (
+              <div className="bg-gray-200 mt-2 p-4 text-gray-600 font-bold">
+                {dataItem.answer}
+              </div>
+            )}
+          </div>
+        ))
+      ) : (
+        <div>
+          <h5>data not found</h5>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Accordion;
---
+++ src/component/Accordian/data.js
@@ -0,0 +1,21 @@
+const sampleData = [
+  {
+    id: "1",
+    question: "What is accordion in component?",
+    answer:
+      "accordion is a user interface element organized and presenting Data",
+  },
+  {
+    id: "2",
+    question: "what are they used for?",
+    answer:
+      "they are employed various contexts, including faqs, product description",
+  },
+  {
+    id: "3",
+    question: "Accordion is a musical instrument?",
+    answer: "The accordion is a musical instrument with a keyboard and bellows",
+  },
+];
+
+export default sampleData;
